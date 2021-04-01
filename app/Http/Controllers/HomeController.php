fd6cd8f8aed8cdda0cc9b49b1ff21e800d82d319
lecbit <artm-marych@rambler.ru>
2021-04-01 22:55:47 +0200

    img_control

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,18 +8,10 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        $products = Product::where('id',5)->first();
-        // foreach($products as $product){
-        //     echo 'Title '. $product->title;
-        //     echo '<br>';
-        //     echo 'Price  '. $product->price;
-        //     echo '<br>';
-        //     echo '-------';
-        //     echo '<br>';
-            
-        // }
-        dd($products);
+        $products = Product::orderBy('created_at')->take(8)->get();
 
-        //return view('home.index');
+        return view('home.index',[
+            'products'=>$products
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'App\Http\Controllers\HomeController@index');
+Route::get('/', 'App\Http\Controllers\HomeController@index');
